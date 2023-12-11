826eea6501bdf669b4f5cae6f20c59cd2b0a2d73
murraysilber <murraysilber@gmail.com>
2023-12-11 16:18:01 +0200

    implemented try-with-resources

--- murray/aoc2021/AoCPuzzle.java
+++ murray/aoc2021/AoCPuzzle.java
@@ -6,88 +6,63 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public abstract class AoCPuzzle {
 
     private String day = "";
-    private List<String> input;
-    private List<String> testInput;
-    
-    public AoCPuzzle(String day) {
+    private List<String> fileInput;
+    private static final String FILE_LOCATION = "resources/day";
+
+    protected AoCPuzzle(String day) {
         this.day = day;
-        setInput(parseInput());
-        setTestInput(parseTestInput());
+        setFileInput(parseInput());
     }
 
-    public List<String> getTestInput() {
-        return testInput;
+    public List<String> getFileInput() {
+        return fileInput;
     }
 
-    public void setTestInput(List<String> testInput) {
-        this.testInput = testInput;
+    private void setFileInput(List<String> input) {
+        this.fileInput = input;
     }
-
-    public List<String> getInput() {
-        return input;
-    }
-
-    private void setInput(List<String> input) {
-        this.input = input;
-    }
-
-    
 
     abstract void solvePart01(List<String> input);
     abstract void solvePart02(List<String> input);
 
     private List<String> parseInput() {
         List<String> input = null;
-        try {
+        try (Stream<String> myStream = Files.lines(Paths.get(FILE_LOCATION + day + ".txt"));) {
             // Trying some NIO
-            input = Files.lines(Paths.get("resources/day" + day + ".txt")).collect(Collectors.toList());
+            input = myStream.collect(Collectors.toList());
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         return input;
     }
 
-    private List<String> parseTestInput() {
-        List<String> input = null;
-        try {
-            // Trying some NIO
-            input = Files.lines(Paths.get("resources/test" + day + ".txt")).collect(Collectors.toList());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return input;
+    public List<Integer> convertToInts(List<String> input) {
+        List<Integer> ints = new ArrayList<>();
+        for (String s : input)
+            ints.add(Integer.parseInt(s));
+        return ints;
     }
 
-    public List<Integer> convertToInts(List<String> input)
-	{
-		List<Integer> ints = new ArrayList<>();
-		for(String s : input)
-			ints.add(Integer.parseInt(s));
-		return ints;
-	}
-
-	public List<Long> convertToLongs(List<String> input)
-	{
-		List<Long> ints = new ArrayList<>();
-		for(String s : input)
-			ints.add(Long.parseLong(s));
-		return ints;
-	}
+    public List<Long> convertToLongs(List<String> input) {
+        List<Long> ints = new ArrayList<>();
+        for (String s : input)
+            ints.add(Long.parseLong(s));
+        return ints;
+    }
 
     public void displayResult(long answer, String part) {
-		System.out.println("Part " + part + ": " + answer);
+        System.out.println("Part " + part + ": " + answer);
     }
 
     public void displayResult(String answer, String part) {
-		System.out.println("Part " + part + ": " + answer);
+        System.out.println("Part " + part + ": " + answer);
     }
-
-    
 
 }
